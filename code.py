e5fc9cb0c9448df53514c7e5e8ca6a7ca2ab8753
rijacobsen <40574798+rijacobsen@users.noreply.github.com>
2021-10-24 21:40:15 +0200

    Update code.py

--- code.py
+++ code.py
@@ -13,9 +13,9 @@ pixels = neopixel.NeoPixel(board.NEOPIXEL, 1) # Instantiate neopixel LED
 ### SETTINGS #########################################################
 
 ### Choose which sensor gain to use
-#sensor.gain = adafruit_tsl2591.GAIN_LOW # 1x gain (for bright light)
+sensor.gain = adafruit_tsl2591.GAIN_LOW # 1x gain (for bright light)
 #sensor.gain = adafruit_tsl2591.GAIN_MED  # 25x gain
-sensor.gain = adafruit_tsl2591.GAIN_HIGH # 428x gain
+#sensor.gain = adafruit_tsl2591.GAIN_HIGH # 428x gain
 #sensor.gain = adafruit_tsl2591.GAIN_MAX # 9876x gain (for dim light)
 
 ### Choose which sensor integration time to use
@@ -29,10 +29,9 @@ sensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_100MS
 ######################################################################
 
 # Main code
-
 while True: #Do the following continuously:
 
-    pixels[0] = (0, 0, 0)  #Turn off the QT Py neopixel to indicate that recording is in progress
+    pixels[0] = (0, 0, 0)  #Turn off the QT Py neopixel to indicate that the code is running
 
     """
     Things that that the sensor can measure:
@@ -45,5 +44,3 @@ while True: #Do the following continuously:
     # Print out the values that should be kept to the serial port
     print("{},{},{}".format(sensor.lux, sensor.visible, sensor.infrared))
     #time.sleep(0.5) #Introduce a delay if you want readings to be taken at a set rate
-
-
