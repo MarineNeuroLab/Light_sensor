f0c36f815a262ef544700ede0989511548181eba
Hotte <hobenhaus@gmail.com>
2024-03-05 11:14:41 +0100

    Update serial_reader.py

--- serial_reader.py
+++ serial_reader.py
@@ -35,79 +35,45 @@ import numpy as np
 ser = serial.Serial(port) #Specify which port to read from 
 ser.flushInput() #This clears the serial buffer so everything is ready to go
 
+
+timestart = datetime.now().strftime('%Y-%m-%d_%H:%M:%S.%f') # Get the current time (as a string)
+current_file = '_'
+
 # Main code:
-while True: #While True, run the following code to record and save temperature values in  the output file specified
+while True: # While True, run the following code to record and save light intensity values in the output file specified
     # Get the current time
-    now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    now_file = datetime.now().strftime('%Y-%m-%d') # This changes only at midnight every day! 
     
     # Create a cvs file with a name based on the current time in the root folder specified 
-    output_file='{}/{}_light.csv'.format(root_folder, now)
+    output_file=f'{root_folder}/{now_file}_light.csv'
+    if output_file != current_file: 
+        # New file name detected 
+        with open(output_file,"w") as f: # Open the file and write header 
+            f.write('Timestamp,Total light (lux),Visible (0-2147483647),Infrared (0-65535)\n') # Add headers to the file. Numbers in brackets are the ranges of possible values
+    current_file = output_file # Prevent writing header until date changes 
+        
+    # Create empty lists to save measurements in (for plotting later)
+    now = datetime.now().strftime('%Y-%m-%d_%H:%M:%S.%f')
+    write_to_file = True
+    try: 
+        ser_bytes = ser.readline() #Read one line from the port
+        if not ser_bytes: 
+            # If nothing arrived via serial ...
+            write_to_file = False 
+        else: 
+            decoded_bytes = ser_bytes.decode('utf-8') # Convert the read data so it's legible
+            decoded_bytes_split = decoded_bytes.strip().split(',') # Strip away the prefix and suffix characters, and split the values using the comma as the separator
+            lux_value = float(decoded_bytes_split[0])
+            visible_value = float(decoded_bytes_split[1])
+            ir_value = float(decoded_bytes_split[2])
+    except: # If "ANY" error occurrs ... this can be made more specific ... 
+        lux_value = np.nan
+        visible_value = np.nan
+        ir_value = np.nan
+        
+    if write_to_file:   
+        with open(output_file,"a") as f: # Open the file and append values 
+            f.write(f'{now},{lux_value},{visible_value},{ir_value}\n')
 
-    #Create empty lists to save measurements in (for plotting later)
-    timevec=[]
-    luxvec=[]
-
-    with open(output_file,"a") as f: #Open the file
-        f.write('Timestamp,Total light (lux),Visible (0-2147483647),Infrared (0-65535)\n') #Add headers to the file. Numbers in brackets are the ranges of possible values
-        timestart = datetime.now().strftime('%Y-%m-%d_%H:%M:%S.%f') #Save the current time as a string, just before measurements start being saved
-        timenow = datetime.now() #Save the current time, which will refresh regularly through the script
-
-        # While the current time is NOT the set newfile_hour/min time, continuously save data to the file
-        while timenow.hour != newfile_hour or timenow.minute != newfile_min:
-            ser_bytes = ser.readline() #Read one line from the port
-            decoded_bytes = ser_bytes.decode('utf-8') #Convert the read data so it's legible
-            decoded_bytes_split = decoded_bytes.strip().split(',') #Strip away the prefix and suffix characters, and split the values using the comma as the separator
-       
-            # Extract the different light values and convert them to floats 
-            # after making sure the received value can be converted (if not that indiates something the value is corrupted and cannot be saved)
-            try: 
-                lux_value = float(decoded_bytes_split[0])
-                visible_value = float(decoded_bytes_split[1])
-                ir_value = float(decoded_bytes_split[2])
-
-            except: #If there is an error (i.e. values above cannot be converted to float), get the current time and save that with NaNs instead
-                now = datetime.now().strftime('%Y-%m-%d_%H:%M:%S.%f')
-                f.write('{},NaN,NaN,NaN\n'.format(now[0:-4]))
-                timevec.append(now[0:-4])
-                luxvec.append('NaN') 
-                print('{} Error occured, values saved as NaNs'.format(now[0:-7]))
-
-            else: #If there is no error, proceed as usual
-                # Get the current time (as a string)
-                now = datetime.now().strftime('%Y-%m-%d_%H:%M:%S.%f')
-            
-                # Save the values in the csv file and in the lists
-                f.write('{},{},{},{}\n'.format(now[0:-4],lux_value,visible_value,ir_value))
-                timevec.append(now[0:-4])
-                luxvec.append(lux_value)    
-
-                # Print out the values in the terminal
-                print('{} | Light: {} lux | Visible: {} | IR: {}'.format(now[0:-7],lux_value,visible_value,ir_value))
-
-                # Introduce the specified delay to only record values roughly every X seconds
-                time.sleep(delay)
-                
-                # Get the current time so the while loop can be evaluated again (is it appropriate to stay in the loop or exit it?)
-                timenow = datetime.now()
-            
-    # Once the while loop has been exited, print a status update in the terminal
-    timeend = datetime.now().strftime('%Y-%m-%d_%H-%M-%S.%f') # Get the current time (as a string)
-    print('Light measurements started saving just after {}, finished saving at {}'.format(timestart[0:-7],timeend[0:-7]))
-    
-    # Create and save a plot of the values collected
-    figure = plt.figure() #Create a plot
-    ax = figure.add_subplot(111)
-    ax.plot(timevec, luxvec,'-')   
-
-    plt.xticks([0, np.floor(len(timevec)/2),len(timevec)-1])
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    
-    plt.xlabel("Time")
-    plt.ylabel("Total light (lux)")
-    plt.savefig('{}/{}_light.png'.format(root_folder,timeend[0:-7]),dpi=300)
-    print('Plot of measurements has been saved')
-    
-    # Include a break to avoid making unnecessary new files when the specified delay is shorter than a minute
-    print('Recording will continue in about 1 minute \n \n')
-    time.sleep(61)
+    # Print out the values in the terminal
+    print('{} | Light: {} lux | Visible: {} | IR: {}'.format(now[0:-7],lux_value,visible_value,ir_value))
